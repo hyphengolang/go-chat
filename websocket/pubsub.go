7191688866391ea33c670399efa5f209e012f534
Kristopher Rahim <kristopherab@gmail.com>
2023-03-07 18:51:43 +0000

    using sync_map

--- websocket/pubsub.go
+++ websocket/pubsub.go
@@ -10,26 +10,26 @@ type PubSub struct {
 	// unregister is the channel that unregisters connections
 	unregister chan *connHandler
 	// connections is the list of connections
-	connections map[string]structures.Set[*connHandler]
+	connections *structures.SyncMap[string, structures.Set[*connHandler]]
 }
 
 func (ps *PubSub) listen() {
 	for {
 		select {
 		case conn := <-ps.register:
-			if conns, ok := ps.connections[conn.channel]; ok {
+			if conns, ok := ps.connections.Load(conn.channel); ok {
 				conns.Add(conn)
 			} else {
-				ps.connections[conn.channel] = structures.NewSet(conn)
+				ps.connections.Store(conn.channel, structures.NewSet(conn))
 			}
 		case conn := <-ps.unregister:
-			if conns, ok := ps.connections[conn.channel]; ok {
+			if conns, ok := ps.connections.Load(conn.channel); ok {
 				conns.Remove(conn)
 
 				close(conn.rcv)
 			}
 		case msg := <-ps.broadcast:
-			if conns, ok := ps.connections[msg.channel]; ok {
+			if conns, ok := ps.connections.Load(msg.channel); ok {
 				for conn := range conns {
 					select {
 					case conn.rcv <- msg:
@@ -48,7 +48,7 @@ func NewPubSub() *PubSub {
 		broadcast:   make(chan *Message, 256),
 		register:    make(chan *connHandler),
 		unregister:  make(chan *connHandler),
-		connections: make(map[string]structures.Set[*connHandler]),
+		connections: structures.NewSyncMap[string, structures.Set[*connHandler]](),
 	}
 	go ps.listen()
 	return &ps
