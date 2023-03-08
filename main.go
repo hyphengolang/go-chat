1b07379e12b08b59f000c281927c4f094f6bf9d8
Kristopher Rahim <kristopherab@gmail.com>
2023-03-08 06:36:21 +0000

    update imports

--- main.go
+++ main.go
@@ -4,61 +4,31 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"go-chat/games"
 	"log"
 	"net/http"
 
-	"github.com/hyphengolang/websockets"
-
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/redis/go-redis/v9"
 )
 
 func run() error {
-	opt, err := redis.ParseURL("redis://localhost:6379")
+	rc, err := newRedisConnection(context.Background(), "redis:6379")
 	if err != nil {
 		return err
 	}
-	rc := redis.NewClient(opt)
 	defer rc.Close()
 
-	if err := rc.Ping(context.Background()).Err(); err != nil {
-		return err
-	}
-
 	mux := chi.NewRouter()
-
-	// simple hello world
-	mux.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
-	})
-
-	c := websockets.NewClient(
-		websockets.WithRedis(rc),
-	)
-
-	c.On("join", func(p websockets.ResponseWriter, b *websockets.Payload) {
-		v, _ := b.Data.MarshalJSON()
-
-		if err := p.Publish(b.Method, v); err != nil {
-			log.Printf("publish err: %v", err)
-		}
-	})
-
-	// join a chat room
-	mux.Get("/play", func(w http.ResponseWriter, r *http.Request) {
-		roomID := r.URL.Query().Get("id")
-
-		// defer removing client from db
-		// this works because ServeHTTP is blocking
-
-		ctx := websockets.NewContext(r.Context(), roomID)
-		r = r.WithContext(ctx)
-
-		c.ServeHTTP(w, r) // blocking statement
-
-		// runs when client disconnects
-		log.Print("client disconnected")
-	})
+	{
+		// simple logging for HTTP requests
+		mux.Use(middleware.Logger)
+	}
+	// mounting services
+	{
+		mux.Mount("/games", newGamesService(rc))
+	}
 
 	srv := http.Server{
 		Addr:    fmt.Sprintf(":%d", port),
@@ -67,6 +37,24 @@ func run() error {
 
 	log.Printf("listening on %s", srv.Addr)
 	return srv.ListenAndServe()
+}
+
+func newRedisConnection(ctx context.Context, url string) (*redis.Client, error) {
+	opt, err := redis.ParseURL(url)
+	if err != nil {
+		return nil, err
+	}
+
+	rc := redis.NewClient(opt)
+	if err := rc.Ping(ctx).Err(); err != nil {
+		return nil, err
+	}
+
+	return rc, nil
+}
+
+func newGamesService(rc *redis.Client) http.Handler {
+	return games.NewService(games.WithRedis(rc))
 }
 
 var port int
